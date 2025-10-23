14344fb8a6b2c4bf2d910b50a742c9b29bbb6e35
SamirTRW <atallahsamir05@gmail.com>
2025-10-23 16:35:45 +0300

    Deploy 2.0

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isProd ? '/chartix-v2.0' : '',
+  assetPrefix: isProd ? '/chartix-v2.0/' : '',
+  images: { 
+    unoptimized: true 
+  },
   eslint: {
     ignoreDuringBuilds: false,
   },
-  images: { unoptimized: true },
   typescript: {
     ignoreBuildErrors: false,
   },
   swcMinify: true,
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
