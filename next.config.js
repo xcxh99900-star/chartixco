d1cc762758c972a1e1384782105f874200fab3d0
SamirTRW <atallahsamir05@gmail.com>
2025-10-23 16:40:37 +0300

    Deploy 2.0

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: isProd ? '/chartixco' : '',
-  assetPrefix: isProd ? '/chartixco/' : '',
-  images: { 
-    unoptimized: true 
-  },
   eslint: {
     ignoreDuringBuilds: false,
   },
+  images: { unoptimized: true },
   typescript: {
     ignoreBuildErrors: false,
   },
   swcMinify: true,
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
